daeb6f52fa62fcfe2a57d31d9e3bc932b7fd3172
Charles <138696615+hungsct1702@users.noreply.github.com>
2024-01-15 13:07:04 +0700

    Update bai_2.c

--- bai_2.c
+++ bai_2.c
@@ -4,18 +4,18 @@ int main()
 {
 
 	int n;
-	float income, tf, ti, pa = 9000000, pd = 3600000;
+	long income, tf, ti, pa = 9000000, pd = 3600000;
     
 
 	printf("Your income of this year: ");
-	scanf("%f", &income);
+	scanf("%ld", &income);
 
     	printf("Number of dependents: ");
     	scanf("%d", &n);
 
     	tf = 12 * (pa + n * pd);
 
-    	printf("Tax-free income: %.2f\n", tf);
+    	printf("Tax-free income: %ld\n", tf);
     	ti = income - tf;
 
     	if (ti <= 0) 
@@ -26,26 +26,26 @@ int main()
 	
 	else if (ti > 0 && ti <= 5000000) 
 	{
-        printf("Taxable income = %.2f\n", ti);
-        printf("Income tax = %.2f\n", ti * 5 / 100);
+        printf("Taxable income = %ld\n", ti);
+        printf("Income tax = %ld\n", ti * 5 / 100);
     	} 
 	
 	else if (ti > 5000000 && ti <= 10000000) 
 	{
-        printf("Taxable income = %.2f\n", ti);
-        printf("Income tax = %.2f\n", 5000000 * 5 / 100 + (ti - 5000000) * 10 / 100);
+        printf("Taxable income = %ld\n", ti);
+        printf("Income tax = %ld\n", 5000000 * 5 / 100 + (ti - 5000000) * 10 / 100);
     	} 
 	
 	else if (ti > 10000000 && ti <= 18000000) 
 	{
-        printf("Taxable income = %.2f\n", ti);
-        printf("Income tax = %.2f\n", 5000000 * 5 / 100 + 5000000 * 10 / 100 + (ti - 10000000) * 15 / 100);
+        printf("Taxable income = %ld\n", ti);
+        printf("Income tax = %ld\n", 5000000 * 5 / 100 + 5000000 * 10 / 100 + (ti - 10000000) * 15 / 100);
     	} 
 	
 	else 
 	{
-        printf("Taxable income = %.2f\n", ti);
-        printf("Income tax = %.2f\n", 5000000 * 5 / 100 + 5000000 * 10 / 100 + 8000000 * 15 / 100 + (ti - 18000000) * 20 / 100);
+        printf("Taxable income = %ld\n", ti);
+        printf("Income tax = %ld\n", 5000000 * 5 / 100 + 5000000 * 10 / 100 + 8000000 * 15 / 100 + (ti - 18000000) * 20 / 100);
     	}
 
 return 0;
